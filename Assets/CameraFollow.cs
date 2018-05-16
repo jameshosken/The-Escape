1923e9ebd23ff910211b4de705eeed7c484970ef
James Hosken <jameswfhosken@gmail.com>
2018-05-16 18:45:57 +0100

    Add basic camera follow mechanism

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] float bounciness = 0.5f;
+
+    Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Vector3.Lerp(transform.position, playerTransform.position, 0.2f); 
+	}
+}
