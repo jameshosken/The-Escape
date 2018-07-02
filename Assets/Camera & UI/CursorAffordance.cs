91c060c07a9c11ef6bf27ea9008cc10fc2bd1e17
James Hosken <jameswfhosken@gmail.com>
2018-07-02 16:45:55 +0100

    Add cursors

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    CameraRaycaster cameraRaycaster;
+
+    [Tooltip ( "Unknown, Walk, Unwalk, Attack")]
+    [SerializeField] Texture2D[] cursors;
+
+    private void Start()
+    {
+        cameraRaycaster = GetComponentInChildren<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        print(cameraRaycaster.layerHit);
+
+        Vector2 cursorHotspot = new Vector2(100,100);
+
+        int cursorIDX = 0;
+
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                cursorIDX = 1;
+                break;
+            case Layer.Unwalkable:
+                cursorIDX = 2;
+                break;
+            case Layer.Enemy:
+                cursorIDX = 3;
+                break;
+
+            default:
+                break;
+        }
+
+        Cursor.SetCursor( cursors[cursorIDX], cursorHotspot, CursorMode.Auto);
+	}
+}
